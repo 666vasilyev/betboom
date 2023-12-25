102f262b2b242144587dff489d002c58b7cc9337
666vasilyev <vasilyev.jpg@yandex.ru>
2023-12-25 23:38:59 +0300

    add parsing tennis odds

--- src/db/crud.py
+++ src/db/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import desc
 import logging
 
-from src.db.models import Odd, Prediction
+from src.db.models import Odd, Prediction, TennisOdd
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,6 +30,38 @@ def add_new_odds(
                 last_odd.first_handicap == new_odd.first_handicap and
                 last_odd.second_handicap == new_odd.second_handicap and
                 last_odd.draw_odd == new_odd.draw_odd
+                ):
+                pass
+            else:
+                session.add(new_odd)
+                logging.info(f'new_odd: {new_odd.match_id}')
+    session.commit()
+
+
+def add_new_tennis_odds(
+        session: Session, 
+        new_odds: list[TennisOdd]
+        ):
+    for new_odd in new_odds:
+        # Получаем последнюю запись для данного match_id
+        last_odd = (
+            session.query(TennisOdd)
+            .filter(TennisOdd.match_id == new_odd.match_id)
+            .order_by(desc(TennisOdd.id))
+            .first()
+        )
+        if last_odd is None:
+            # Если нет предыдущих записей с данным match_id, добавляем новую запись
+            session.add(new_odd)
+            logging.info(f'new_odd(cause last doesnt exist): {new_odd.match_id}')
+        else:
+            # Если есть предыдущая запись, проверяем коэффициенты
+            if (last_odd.p1 == new_odd.p1 and 
+                last_odd.p2 == new_odd.p2 and
+                last_odd.fora_f1 == new_odd.fora_f1 and
+                last_odd.fora_f2 == new_odd.fora_f2 and
+                last_odd.total_b == new_odd.total_b and 
+                last_odd.total_m == new_odd.total_m
                 ):
                 pass
             else:
--- src/db/models.py
+++ src/db/models.py
@@ -9,14 +9,20 @@ class Odd(Base):
     __tablename__ = "Odds"
     id: Mapped[int] = mapped_column(primary_key=True)
     time: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.utcnow)
+
     match_id: Mapped[int] = mapped_column()
+
     first_team: Mapped[str] = mapped_column(nullable=False)
     second_team: Mapped[str] = mapped_column(nullable=False)
+
     first_odd: Mapped[float] = mapped_column()
     second_odd: Mapped[float] = mapped_column()
+
     draw_odd: Mapped[float] = mapped_column(server_default=None, nullable=True)
+
     first_handicap: Mapped[str] = mapped_column(nullable=True)
     second_handicap: Mapped[str] = mapped_column(nullable=True)
+
     sport_name: Mapped[str] = mapped_column(nullable=False)
 
 
@@ -24,8 +30,32 @@ class Prediction(Base):
     __tablename__ = "Predictions"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     time: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.utcnow)
+
     first_team: Mapped[str] = mapped_column()
     second_team: Mapped[str] = mapped_column()
+
     winner: Mapped[str] = mapped_column()
+
     bet: Mapped[str] = mapped_column()
     ratio: Mapped[str] = mapped_column()
+
+
+class TennisOdd(Base):
+    __tablename__ = "TennisOdds"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    match_id: Mapped[int] = mapped_column
+    time: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.utcnow)
+
+    first_player: Mapped[str] = mapped_column()
+    second_player: Mapped[str] = mapped_column()
+
+    p1: Mapped[float] = mapped_column()
+    p2: Mapped[float] = mapped_column()
+
+    fora_f1: Mapped[str] = mapped_column()
+    fora_f2: Mapped[str] = mapped_column()
+
+    
+    total_b: Mapped[str] = mapped_column()
+    total_m: Mapped[str] = mapped_column()
+
